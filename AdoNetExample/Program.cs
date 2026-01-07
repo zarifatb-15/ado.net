ba38ec3c97847d6bc670d395b8f0cd8c21f32199
zarifatb-15 <zarifatb@code.edu.az>
2026-01-07 17:15:23 +0400

    adonet practice

--- AdoNetExample/Program.cs
+++ AdoNetExample/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
     //ado.net example
+    
+    //remote qosulma stringi
+    //"server=localhost;database=TestDB;user id=sa;password=your_password;" 
+    
+    //local qosulma stringi
+    "server=localhost;database=TestDB;trusted_connection=true; trusted_server_certificate=true;"
+    
+    
+    
     }
 }
